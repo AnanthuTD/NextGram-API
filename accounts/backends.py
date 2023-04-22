63d2c230b04d4b4cbccc1fa59cf56443bad41d96
AnanthuTD <ananthutd2021@gmail.com>
2023-04-22 15:51:11 +0530

    added get_profile

--- accounts/backends.py
+++ accounts/backends.py
@@ -3,7 +3,7 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 
-from accounts.models import User,Profile
+from accounts.models import User, Profile
 logger = logging.getLogger(__name__)
 
 
@@ -11,17 +11,18 @@ class UsernameEmailPhoneBackend(ModelBackend):
     def authenticate(self, request, phone_email_username=None, password=None, **kwargs):
         user_model = get_user_model()
         try:
-            user = user_model.objects.get(Q(username=phone_email_username) | Q(email=phone_email_username) | Q(profile__phone=phone_email_username))
-            # logger.debug('user found', user)
+            user = user_model.objects.get(Q(username=phone_email_username) | Q(
+                email=phone_email_username) | Q(profile__phone=phone_email_username))
             if user.check_password(password) and self.user_can_authenticate(user):
+                profile = self.get_profile(user.id)
+                user.profile = profile
                 return user
         except user_model.DoesNotExist:
             return None
-        
-    def get_profile(user_id):
+
+    def get_profile(self, id):
         try:
-            profile = Profile.objects.get(id=user_id)
+            profile = Profile.objects.get(user_id=id)
             return profile
         except Profile.DoesNotExist:
             return None
-
