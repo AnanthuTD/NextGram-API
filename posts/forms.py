26d8564dab4ba96351409080d57cab19694ee5c2
AnanthuTD <ananthutd2021@gmail.com>
2023-05-01 20:33:44 +0530

    created forms for posting

---
+++ posts/forms.py
@@ -0,0 +1,44 @@
+from typing import Any
+from django import forms
+from .models import Post
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class PostForm(forms.ModelForm):
+
+    user=None
+
+    class Meta:
+        model = Post
+        fields = [
+                  'file',
+                  'like',
+                  'shares',
+                  'description',
+                  'hash_tag',
+                  'mentions',
+                  'location',
+                  ]
+
+    def __init__(self, *args, **kwargs):
+        user_id = kwargs.pop('user_id', None)
+        super().__init__(*args, **kwargs)
+        if user_id:
+            self.user = User.objects.get(id=user_id)
+           
+    def clean(self):
+        cleaned_data = super(PostForm, self).clean()
+        hash_tag = cleaned_data.get('hash_tag')
+
+        if hash_tag and len(hash_tag) > 3:
+            raise forms.ValidationError('You can only use up to 3 hash tags.')
+        return cleaned_data
+
+    def save(self, commit=True) -> Any:
+        post = super().save(commit=False)
+        post.user = self.user
+        if commit:
+            post.save()
+        return post
