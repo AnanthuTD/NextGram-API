c9c1a914c7f910f184ba4226f36690763e4290f6
AnanthuTD <ananthutd2021@gmail.com>
2023-05-01 20:35:33 +0530

    added rename_media() and null true in like and share

--- posts/models.py
+++ posts/models.py
@@ -2,15 +2,16 @@ import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.postgres.fields import ArrayField
+from posts.utils.rename_media import rename_media
 
 User = get_user_model()
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post_id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    post = models.CharField(unique=True, max_length=300, blank=False, null=False)
-    like = ArrayField(models.CharField(max_length=255))
-    shares = ArrayField(models.CharField(max_length=255))
+    file = models.FileField(upload_to=rename_media)
+    like = ArrayField(models.CharField(max_length=255), null=True, blank=True)
+    shares = ArrayField(models.CharField(max_length=255), null=True, blank=True)
     description = models.TextField(blank=True)
     hash_tag = ArrayField(models.CharField(max_length=255), blank=True, null=True)
     mentions = ArrayField(models.CharField(max_length=255), blank=True, null=True)
