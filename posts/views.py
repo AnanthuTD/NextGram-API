05efe6d20b337864009bc46d743b95970cb4efe4
AnanthuTD <ananthutd2021@gmail.com>
2023-05-01 20:39:39 +0530

    added post view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .forms import PostForm
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def post(request):
+    file = request.FILES
+    caption = request.POST.get('caption')
+    location = request.POST.get('location')
+
+    print('user : ',request.user.id)
+
+    print(file, caption, location)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, user_id=request.user.id)
+
+        if form.is_valid():
+            post = form.save()
+            return JsonResponse({'status':True})
+        else:
+            print(form.errors)
+            return JsonResponse({'status':False})
+    else:
+        return JsonResponse({'status':False})
+    
