ddda5b6ce95051643386c630d6f23092183705e6
AnanthuTD <ananthutd2021@gmail.com>
2023-05-02 19:31:19 +0530

    added get method for post view

--- post/views.py
+++ post/views.py
@@ -1,6 +1,8 @@
+import logging
 from django.http import JsonResponse
 from .forms import PostForm
 from django.views.decorators.csrf import csrf_exempt
+
 
 def post(request):
 
@@ -9,10 +11,24 @@ def post(request):
 
         if form.is_valid():
             post = form.save()
-            return JsonResponse({'status':True})
+            return JsonResponse({'status': True})
         else:
-            print("posting errors : ",form.errors)
-            return JsonResponse({'status':False})
-    else:
-        return JsonResponse({'status':False})
-    
+            print("posting errors : ", form.errors)
+            return JsonResponse({'status': False})
+
+    elif request.method == "GET":
+        user = request.user
+
+        try:
+            posts = list(user.post_set.all().values("post_id",
+                                                    'file',
+                                                    'like',
+                                                    'shares',
+                                                    'caption',
+                                                    'hash_tag',
+                                                    'mentions',
+                                                    'location',))
+            return JsonResponse({'status': True, 'posts': posts})
+        except Exception as e:
+            logging.error(e, exc_info=True)
+            return JsonResponse({'status': False, 'message': str(e)})
