80df4f09c4e3e7517b046b0ab66215be9a7e2c52
AnanthuTD <ananthutd2021@gmail.com>
2023-05-09 19:43:03 +0530

    added post count and changed default profile pic

--- accounts/models.py
+++ accounts/models.py
@@ -11,7 +11,10 @@ class Profile(models.Model):
     phone = models.CharField(max_length=20, blank=True, null=True)
     bio = models.TextField(blank=True)
     profile_img = models.ImageField(
-        upload_to='profile_images', default='default-profile-pic.svg')
+        upload_to='profile_images', default='default_profile.png')
     location = models.CharField(max_length=100, blank=True)
     id_user = models.UUIDField(unique=True, default=uuid.uuid4)
-
+    gender = models.CharField(max_length=20, blank=True)
+    website = models.URLField(blank=True)
+    following = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+    post_count = models.PositiveIntegerField(default=0)
