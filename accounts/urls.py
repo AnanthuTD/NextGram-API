1360704e3622db7d1e0665fe8c50166afda46e14
AnanthuTD <ananthutd2021@gmail.com>
2023-05-09 19:44:20 +0530

    added profile and getprofile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('login/', views.login_view, name="login"),
-    path('logout/', views.logout_view , name="logout")
+    path('logout/', views.logout_view , name="logout"),
+    path('profile/', views.profile, name="profile"),
+    path('get_profile/<username>/', views.get_profile, name="get_profile"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
-import traceback
+
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.http import require_POST, require_GET
 import json
-from django.core import serializers
 from .forms import SignupForm
 from django.contrib.auth import authenticate, login, logout
+from .models import Profile
+from django.contrib.auth.models import User
+from django.db.models import F
 
 
 @require_POST
@@ -20,12 +22,12 @@ def signup(request):
     if form.is_valid():
         try:
             # Save the user data
-            user = form.save()
+            form.save()
 
             phone_email_username = data["username"]
             password = data["password1"]
 
-            newuser = authenticate(
+            authuser = authenticate(
                 request, phone_email_username=phone_email_username, password=password)
 
             # Remove session data
@@ -35,38 +37,29 @@ def signup(request):
             request.session.create()
 
             # set current user session data
-            try:
-                login(request, newuser)
-            except Exception as e:
-                print("error logging in")
-                traceback.print_exc()
-                raise ValueError("intentional")
+            login(request, authuser)
+
+            response = JsonResponse({
+                'status': True,
+                'user': serialize_to_dict(authuser)}) # type: ignore
+
+            id_user = authuser.profile.id_user  # type: ignore
+            response.set_cookie('user', value=id_user,
+                                expires=request.session.get_expiry_date(), samesite='Lax')
 
             # Return a JSON response with the user data
-            return JsonResponse({
-                'success': True,
-                'user': {
-                    'id': user.id,
-                    'username': user.username,
-                    'first_name': user.first_name,
-                    'last_name': user.last_name,
-                    'email': user.email or "",
-                    'phone': form.phone or "",
-                    # 'id_user': form.cleaned_data['id_user'],
-                    'bio': form.cleaned_data.get('bio', ''),
-                    'location': form.cleaned_data.get('location', ''),
-                }})
+            return response
         except:
             # Server_error(database)
             return JsonResponse({
-                'success': False,
+                'status': False,
                 'errors': {"server_error": True},
             })
 
     else:
         # Return a JSON response with the form errors
         return JsonResponse({
-            'success': False,
+            'status': False,
             'errors': form.errors,
         })
 
@@ -82,7 +75,7 @@ def login_view(request: HttpRequest):
         password = data['password1']
 
         user = authenticate(
-            request, phone_email_username=phone_email_username, password=password)
+            request, phone_email_username=phone_email_username, password=password) 
 
         if not user:
             return JsonResponse({'status': False, 'errors': {}})
@@ -93,39 +86,88 @@ def login_view(request: HttpRequest):
 
         response = JsonResponse({
             'status': True,
-            'user': serialize_to_dict(user)})
+            'user': serialize_to_dict(user)}) # type: ignore
 
-        id_user = user.profile.id_user
+        id_user = user.profile.id_user  # type: ignore
         response.set_cookie('user', value=id_user,
-                            expires=request.session.get_expiry_date(),samesite='Lax')
+                            expires=request.session.get_expiry_date(), samesite='Lax', domain='localhost')
 
         return response
 
     elif (request.method == 'GET'):
-        if request.user.is_authenticated:
+        if request.user.is_authenticated and request.user.profile:  # type: ignore
             return JsonResponse({
                 'status': True,
-                'user': serialize_to_dict(request.user)})
-        return JsonResponse({'status': False})
+                'user': serialize_to_dict(request.user)}) # type: ignore
+        else:
+            logout_view(request)
+            return JsonResponse({'status': False})
+
     else:
         return JsonResponse({'status': False})
 
 
-def serialize_to_dict(user):
+def serialize_to_dict(user: User):
+    profile: Profile = user.profile  # type: ignore
+    following_ids = list(profile.following.values_list('id_user', flat=True))
+    followers_ids = list(profile.followers.values_list(  # type: ignore
+        'id_user', flat=True))
+
     return {
-        # 'id': user.id,
+        'id_user': profile.id_user,
         'username': user.username,
         'first_name': user.first_name,
         'last_name': user.last_name,
         'email': user.email,
-        'phone': user.profile.phone or '',
-        'bio': user.profile.bio or '',
-        'location': user.profile.location or '',
+        'phone': profile.phone or '',
+        'bio': profile.bio or '',
+        'location': profile.location or '',
+        'gender': profile.gender,
+        'website': profile.website,
+        'profile_img': profile.profile_img.url,
+        'followers': followers_ids,
+        'following': following_ids,
+        'post_count': profile.post_count,
     }
 
 
+@require_GET
 def logout_view(request: HttpRequest):
     logout(request)
     response = JsonResponse({'status': True})
     response.delete_cookie('user')
     return response
+
+
+@require_POST
+def profile(request: HttpRequest):
+    profile: Profile = request.user.profile  # type: ignore
+
+    data = json.loads(request.body)
+    profile.bio = data['bio']  # type: ignore
+    profile.gender = data['gender']  # type: ignore
+    profile.website = data['website']  # type: ignore
+    profile.save()
+    return JsonResponse({'status': True})
+
+
+def get_profile(request: HttpRequest, username: str):
+    user = User.objects.get(username=username)
+    profile: Profile = user.profile  # type: ignore
+    following_ids = list(profile.following.values_list('id_user', flat=True))
+    followers_ids = list(profile.followers.values_list(  # type: ignore
+        'id_user', flat=True))
+
+    profile_res = {'id_user': profile.id_user,
+                   'username': user.username,
+                   'first_name': user.first_name,
+                   'last_name': user.last_name,
+                   'bio': profile.bio or '',
+                   'location': profile.location or '',
+                   'website': profile.website,
+                   'profile_img': profile.profile_img.url,
+                   'followers': followers_ids,
+                   'following': following_ids,
+                   'post_count': profile.post_count, }
+
+    return JsonResponse({'status': True, 'profile': profile_res})
