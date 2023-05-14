edbb97cc8cc50e374b2841ceea8932e02cf9663c
AnanthuTD <ananthutd2021@gmail.com>
2023-05-14 23:51:04 +0530

    added a new path to post view , which recives username of other_users . it is used to get the posts of a particular user which the current user is viewing.

--- post/urls.py
+++ post/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post, name="post"),
+    path('<str:other_user>/posts/', views.post, name='fetch_data_other'),
     path('allPost/', views.allPost, name="allPost"),
     path('<uuid:post_id>/like/', views.like, name="like"),
     path('<uuid:post_id>/dislike/', views.dislike, name="dislike"),
