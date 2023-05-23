f5748015545f04fe2f504674259a95c3d28078e8
AnanthuTD <ananthutd2021@gmail.com>
2023-05-23 22:03:32 +0530

    removed unnecessory imports

--- chat/views.py
+++ chat/views.py
@@ -4,7 +4,6 @@ from .models import Chat, Conversation
 from django.db.models import Q, Max
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
-from . import consumers
 
 
 def conversations(request: HttpRequest):
