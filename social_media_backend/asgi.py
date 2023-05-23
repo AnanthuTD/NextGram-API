b635f08917f7bde9508eb751011fdf25cf2db5ba
AnanthuTD <ananthutd2021@gmail.com>
2023-05-23 22:04:41 +0530

    added urlroute for commentConsumer

--- social_media_backend/asgi.py
+++ social_media_backend/asgi.py
@@ -5,6 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 import chat.routing
+import post.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_backend.settings')
 
@@ -12,7 +13,7 @@ application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns + post.routing.websocket_urlpatterns))
         ),
     }
 )
