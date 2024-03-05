32e29e62d6e79b10d62cb6d13f5f55ad61d19856
AnanthuTD <ananthutd2021@gmail.com>
2024-03-06 00:11:33 +0530

    using .env

--- social_media_backend/settings.py
+++ social_media_backend/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import logging
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,14 +110,20 @@ WSGI_APPLICATION = 'social_media_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wowgram',
-        'USER': 'postgres',
-        'PASSWORD': '#BreakRules.',
-        'HOST': os.environ.get('DB_HOST', 'localhost'),   
-        'PORT': '5432',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': os.environ.get('DB_HOST', DB_HOST),   
+        'PORT': DB_PORT,
     }
 }
 
